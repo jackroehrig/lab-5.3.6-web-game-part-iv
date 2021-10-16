9685af39bdf3720d28f1df64515fc2d694bfcce8
jackroehrig <jack.roe13@gmail.com>
2021-10-16 09:28:04 -0500

    Made the character move using arrow keys among other things

--- move.js
+++ move.js
@@ -6,7 +6,57 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, handleDirectionChange){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        function moveCharacter(){
+            if(direction === 'west' && x >= 0){
+                x--;
+            } else if(direction === 'north' && y <= window.innerHeight-element.offsetHeight){
+                y++;
+            } else if(direction === 'east' && x <= window.innerWidth-element.offsetWidth){
+                x++;
+            } else if(direction === 'south' && y >= 105){
+                y--;
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+        }
+
+        setInterval(moveCharacter, 1)
+
+        document.addEventListener('keydown', e => {
+            if(e.repeat) return;
+
+            if(e.key === 'ArrowLeft'){
+                direction = 'west'
+            } else if(e.key === 'ArrowUp'){
+                direction = 'north'
+            } else if(e.key === 'ArrowRight'){
+                direction = 'east'
+            } else if(e.key === 'ArrowDown'){
+                direction = 'south'
+            }
+            if(handleDirectionChange){
+                handleDirectionChange(direction)
+            }
+        })
+
+        document.addEventListener('keyup', e => {
+            direction = null;
+            if(handleDirectionChange){
+                handleDirectionChange(direction)
+            }
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
--- newItem.js
+++ newItem.js
@@ -1,5 +1,6 @@
 function newItem(url){
     let item = newImage(url)
+    item.style.zIndex = 10;
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
